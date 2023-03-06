09b9f1a9c5a1e5ba0398e93f56da225071c0766d
unknown <anthatimasthan785@gmail.com>
2023-03-06 20:33:35 +0530

    upload the eaitenth project

---
+++ src/main/java/codejava/Codejava.java
@@ -0,0 +1,13 @@
+package codejava;
+
+public class Codejava {
+	
+	public static String welcomeMule() {
+		return "welcom to mule soft";
+	}
+	
+	public static String StuName(String fname,String lname) {
+		return fname + "-" + lname;
+	}
+
+}
